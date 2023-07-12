d2cfe395a7dca3d823b3c03e84e751a21bfeeafc
Justen Schilinski <justenschilinski34@hotmail.com>
2023-07-11 22:01:25 -0400

    Added d3 library and api for project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 
 function App() {
+  const req = () => {
+    fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
+      .then(response => {
+        return response.json()
+      })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
