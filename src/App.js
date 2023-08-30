96d18d3c6bbed15950bf05f19a1cf7197f313d45
Justen Schilinski <justenschilinski34@hotmail.com>
2023-08-30 10:45:10 -0400

    Added y axis

--- src/App.js
+++ src/App.js
@@ -32,16 +32,17 @@ const App = () => {
     const minY = d3.min(data, (d) => d[1]);
     const maxY = d3.max(data, (d) => d[1]);
 
+    console.log(maxY)
+
     const xScale = d3.scaleLinear()
                     .domain([0, maxX])
                     .range([p, w - p]);
 
     const yScale = d3.scaleLinear()
                     .domain([minY, maxY])
-                    .range([h - p, p])
+                    .range([h - p, p]);
                   
-
-    const svg = d3.select(".chart-svg")
+    const svg = d3.select(".chart-svg");
 
       svg.selectAll("rect")
          .data(data)
@@ -50,7 +51,14 @@ const App = () => {
          .attr("x", (d, i) => xScale(i))
          .attr("y", (d) => yScale(d[1]))
          .attr("width", 3)
-         .attr("height", (d) => d[1])
+         .attr("height", (d) => d[1]);
+
+    const yAxis = d3.axisLeft(yScale);
+
+      svg.append("g")
+         .attr("transform", "translate(" + 20 + ",0)")
+         .style("padding-left", "20px")
+         .call(yAxis);
     
   }, [data]); 
 
