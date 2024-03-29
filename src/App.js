83325752bf72f14970f0d4072fbfd495cfb2c59d
Justen Schilinski <justenschilinski34@hotmail.com>
2024-03-28 21:44:26 -0400

    Testing x-axis issue

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ const App = () => {
 
       svg.append("g")
          .attr("id", "x-axis")
-         .attr("transform", "translate(-4," + h + ")")
+         .attr("transform", "translate(-8," + h + ")")
          .call(xAxis);
       
   }, [data]); 
