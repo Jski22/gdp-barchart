e9d613adb0117adbc61ab9912faad4e984cb897a
Justen Schilinski <justenschilinski34@hotmail.com>
2024-07-22 22:16:39 -0400

    Changed new x-axis coordinates

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ const App = () => {
     <div ref={svgRef} className="App">
       <div className="tooltip" id="tooltip"></div>
       <svg className="chart-svg" width="1200px">
-        <g id="x-axis" transform="translate(0, 20)" xScale={value}>
+        <g id="x-axis" transform="translate(0, 40)" xScale={value}>
           <axisBottom></axisBottom>
           
         </g>
