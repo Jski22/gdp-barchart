c16921181baa64f9941f23111f0023d3513fd99e
Justen Schilinski <justenschilinski34@hotmail.com>
2024-07-31 22:36:36 -0400

    Added inner height property to bottom axis

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ const App = () => {
       <div className="tooltip" id="tooltip"></div>
       <svg className="chart-svg" width="1200px">
         <g id="x-axis" transform="translate(0, 40)">
-          <AxisBottom  xScale={value}/>
+          <AxisBottom  xScale={value} innerHeight={innerHeight} />
           
         </g>
       </svg>
